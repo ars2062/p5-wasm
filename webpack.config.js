0a88f91f673f4007db095ce2e44e79a3a40865b4
Arshia Moghaddam <ars2062@gmail.com>
2022-04-07 22:52:45 +0430

    add: dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 /** @type {import('webpack').Configuration} */
 const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-const generalConfig = {
+require('dotenv').config()
+exports.default = {
   entry: "./src/index.ts",
-  externals: {
-    p5: "p5",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "p5.wasm.js",
+    clean: true,
   },
+  mode: process.env.MODE || "production",
   module: {
     rules: [
       {
@@ -26,48 +27,10 @@ const generalConfig = {
   resolve: {
     extensions: [".ts", ".js"],
   },
+  externals: {
+    p5: "p5",
+  },
   experiments: {
     asyncWebAssembly: true,
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyPlugin({
-      patterns: [{ from: "./src/index.html", to: "" }],
-    }),
-  ],
-}
-
-const nodeConfig = {
-  target: 'node',
-  output: {
-    path: path.resolve(__dirname, "dist", "node"),
-    filename: "p5.wasm.js",
-    libraryTarget: 'umd',
-    libraryExport: 'default'
-  },
-}
-
-const browserConfig = {
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, "dist", "web"),
-    filename: "p5.wasm.js",
-    libraryTarget: 'umd',
-    libraryExport: 'default'
-  },
-}
-
-module.exports = (env, argv) => {
-  if(argv.mode === 'development') {
-    generalConfig.devtool = 'cheap-module-source-map';
-  } else if (argv.mode === 'production') {
-
-  } else {
-    throw new Error('please specify "env" flag')
-  }
-
-  Object.assign(nodeConfig, generalConfig)
-  Object.assign(browserConfig, generalConfig)
-
-  return [nodeConfig, browserConfig]
-}
+};
