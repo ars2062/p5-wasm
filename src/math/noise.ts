9a6822802f517536eedb76e7b38b0832c995dc83
Arshia Moghaddam <ars2062@gmail.com>
2022-04-07 17:24:49 +0430

    feat: calculation noise

--- src/math/noise.ts
+++ src/math/noise.ts
@@ -10,3 +10,4 @@ p5.prototype.noise = function (x, y = 0, z = 0) {
     return Module._noise(x, y, z)
 };
 p5.prototype.noiseDetail = Module._noiseDetail;
+p5.prototype.noiseSeed = Module._noiseSeed;
